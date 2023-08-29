e1d0432f9e0bd9dcb2d25441e7f73d3869238d25
Rostyslav Kostylov <rostik.costylyow@gmail.com>
2023-08-29 22:53:25 +0200

    added new script

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__item-percent'),
+    lines = document.querySelectorAll('.skills__item-scale');
+
+counters.forEach(  (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
